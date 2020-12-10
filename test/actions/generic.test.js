5d928b22aa0559636389abeb071990db9b89a807
sandeep-paliwal <spaliwal@adobe.com>
2020-12-10 12:12:30 +0530

    Update generic.test.js

--- test/actions/generic.test.js
+++ test/actions/generic.test.js
@@ -22,7 +22,7 @@ beforeEach(() => {
   mockLoggerInstance.debug.mockReset()
   mockLoggerInstance.error.mockReset()
 })
-// test comment
+// test comment 1
 const fakeParams = { __ow_headers: { authorization: 'Bearer fake' } }
 describe('generic', () => {
   test('main should be defined', () => {
