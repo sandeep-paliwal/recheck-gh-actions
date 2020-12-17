bb45e05106abe8c9b5718419c75db0c1361e9ba3
sandeep-paliwal <spaliwal@adobe.com>
2020-12-17 19:20:26 +0530

    Update execa.js

--- execa.js
+++ execa.js
@@ -1,16 +1,10 @@
 const execa = require('execa')
-console.log('about to run test')
+
 test()
-console.log('completed test')
 
 function test() {
-  const child1 = execa.command('npm list', {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
-    cwd: process.cwd(),
-    preferLocal: true
-  })
-  
-  const child2 = execa.command('jest --passWithNoTests ./test', {
+ 
+  const child = execa.command('jest --passWithNoTests ./test', {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
     cwd: process.cwd(),
     preferLocal: true
